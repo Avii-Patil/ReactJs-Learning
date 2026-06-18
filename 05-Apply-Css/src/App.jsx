49aa35ea0d8514d0df77dca15018e27bb070ce90
Avishkar Khatik <avishkarkhatik15@gmail.com>
2026-06-18 21:59:23 +0530

    Add React Day 17 folder

---
+++ 05-Apply-Css/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Card from './components/Card/Card.jsx'
+import Button from './components/Button/Button.jsx'
+
+const App = () => {
+  return (
+    <div>
+      <Button />
+      <Card />
+    </div>
+  )
+}
+
+export default App
---
+++ 05-Apply-Css/src/components/Card/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import styles from "./Card.module.css";
+const Card = () => {
+  return (
+    <div className={styles.card}>This is a simple Card</div>
+  )
+}
+
+export default Card
